05454718c15b01cbed9fa202ebc65afb5c2b478c
Acquisition Computer <acquisition@D5K1X52.ad.nyu.edu>
2021-07-14 10:47:39 -0400

    Add support for videos to calibrate_camera.py diff --git a/camera_params/cam1/camera_matrix.npy b/camera_params/cam1/camera_matrix.npy deleted file mode 100644 index a71261d..0000000 Binary files a/camera_params/cam1/camera_matrix.npy and /dev/null differ diff --git a/camera_params/cam1/distortions.npy b/camera_params/cam1/distortions.npy deleted file mode 100644 index 46816d1..0000000 Binary files a/camera_params/cam1/distortions.npy and /dev/null differ diff --git a/camera_params/cam1/rotations.npy b/camera_params/cam1/rotations.npy deleted file mode 100644 index 0791b1c..0000000 Binary files a/camera_params/cam1/rotations.npy and /dev/null differ diff --git a/camera_params/cam1/translations.npy b/camera_params/cam1/translations.npy deleted file mode 100644 index 73254e8..0000000 Binary files a/camera_params/cam1/translations.npy and /dev/null differ diff --git a/scripts/calibrate_camera.py b/scripts/calibrate_camera.py index 637ce0b..27c9378 100644 --- a/scripts/calibrate_camera.py +++ b/scripts/calibrate_camera.py @@ -6,7 +6,10 @@ import numpy as np
     image_dir = 'calibration'
    -image_paths = sorted([path.join(image_dir, filename) for filename in os.listdir(image_dir)])
    +image_paths = sorted([path.join(image_dir, filename) for filename in os.listdir(image_dir) if filename.endswith('.png')])
    +
    +use_vid = True
    +vid_path = path.join(image_dir, 'calibrate.avi')
    
     output_dir = 'camera_params/cam_a'
     if not path.exists(output_dir):
    @@ -27,17 +30,23 @@ image_points = list()
     # The origin and orientation of the world coordinates is arbitrary for our purposes
     world_frame = np.zeros((1, board_size[0] * board_size[1], 3), np.float32)
     world_frame[0, :, :2] = np.mgrid[0:board_size[0], 0:board_size[1]].T.reshape(-1, 2)
    -prev_image_shape = None
    +# image_shape = None
    +num_samples = 0
    +
    
    -for image_path in image_paths:
    -    img = cv2.imread(image_path)
    -    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
    +def process(img):
    +    # img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
    +    if len(img.shape) == 3:
    +        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
    +    else:
    +        gray = img
    
    +    global image_shape
    +    image_shape = gray.shape
         # Finds the locations of chessboard corners in image space
         ret, corners = cv2.findChessboardCorners(gray,
                 board_size,
    -            cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK +
    -            cv2.CALIB_CB_NORMALIZE_IMAGE)
    +            cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
         if ret:
             world_points.append(world_frame)
             corners_refined = cv2.cornerSubPix(gray,
    @@ -48,16 +57,33 @@ for image_path in image_paths:
             image_points.append(corners_refined)
    
    +if not use_vid:
    +    num_samples = len(image_paths)
    +    for image_path in image_paths:
    +        img = cv2.imread(image_path)
    +        process(img)
    +else:
    +    cap = cv2.VideoCapture(vid_path)
    +    while cap.isOpened():
    +        ret, frame = cap.read()
    +        if not ret:
    +            break
    +        num_samples += 1
    +        process(frame)
    +
    +
     k_matrix = np.zeros((3, 3))
     d_matrix = np.zeros((4, 1))
     # Placeholders for rotation and translation vectors
    -r_vecs = [np.zeros((1, 1, 3), dtype=float) for _ in range(len(image_paths))]
    -t_vecs = [np.zeros((1, 1, 3), dtype=float) for _ in range(len(image_paths))]
    +r_vecs = [np.zeros((1, 1, 3), dtype=float) for _ in range(num_samples)]
    +t_vecs = [np.zeros((1, 1, 3), dtype=float) for _ in range(num_samples)]
    
    +print(criteria)
    +print(image_shape[::-1])
     cv2.fisheye.calibrate(
         world_points,
         image_points,
    -    gray.shape[::-1],
    +    image_shape[::-1],
         k_matrix,
         d_matrix,
         r_vecs,
    @@ -66,6 +92,7 @@ cv2.fisheye.calibrate(
         criteria
     )
    
    +
     np.save(path.join(output_dir, 'K.npy'), k_matrix)
     np.save(path.join(output_dir, 'D.npy'), d_matrix)

--- scripts/calibrate_camera.py
+++ scripts/calibrate_camera.py
@@ -6,7 +6,10 @@ import numpy as np
 
 
 image_dir = 'calibration'
-image_paths = sorted([path.join(image_dir, filename) for filename in os.listdir(image_dir)])
+image_paths = sorted([path.join(image_dir, filename) for filename in os.listdir(image_dir) if filename.endswith('.png')])
+
+use_vid = True
+vid_path = path.join(image_dir, 'calibrate.avi')
 
 output_dir = 'camera_params/cam_a'
 if not path.exists(output_dir):
@@ -27,17 +30,23 @@ image_points = list()
 # The origin and orientation of the world coordinates is arbitrary for our purposes
 world_frame = np.zeros((1, board_size[0] * board_size[1], 3), np.float32)
 world_frame[0, :, :2] = np.mgrid[0:board_size[0], 0:board_size[1]].T.reshape(-1, 2)
-prev_image_shape = None
+# image_shape = None
+num_samples = 0
 
-for image_path in image_paths:
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+def process(img):
+    # img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+    if len(img.shape) == 3:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else:
+        gray = img
+
+    global image_shape
+    image_shape = gray.shape
     # Finds the locations of chessboard corners in image space
     ret, corners = cv2.findChessboardCorners(gray,
             board_size,
-            cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK +
-            cv2.CALIB_CB_NORMALIZE_IMAGE)
+            cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
     if ret:
         world_points.append(world_frame)
         corners_refined = cv2.cornerSubPix(gray,
@@ -48,16 +57,33 @@ for image_path in image_paths:
         image_points.append(corners_refined)
 
 
+if not use_vid:
+    num_samples = len(image_paths)
+    for image_path in image_paths:
+        img = cv2.imread(image_path)
+        process(img)
+else:
+    cap = cv2.VideoCapture(vid_path)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        num_samples += 1
+        process(frame)
+
+
 k_matrix = np.zeros((3, 3))
 d_matrix = np.zeros((4, 1))
 # Placeholders for rotation and translation vectors
-r_vecs = [np.zeros((1, 1, 3), dtype=float) for _ in range(len(image_paths))]
-t_vecs = [np.zeros((1, 1, 3), dtype=float) for _ in range(len(image_paths))]
+r_vecs = [np.zeros((1, 1, 3), dtype=float) for _ in range(num_samples)]
+t_vecs = [np.zeros((1, 1, 3), dtype=float) for _ in range(num_samples)]
 
+print(criteria)
+print(image_shape[::-1])
 cv2.fisheye.calibrate(
     world_points,
     image_points,
-    gray.shape[::-1],
+    image_shape[::-1],
     k_matrix,
     d_matrix,
     r_vecs,
@@ -65,6 +91,7 @@ cv2.fisheye.calibrate(
     cal_flags,
     criteria
 )
+
 
 np.save(path.join(output_dir, 'K.npy'), k_matrix)
 np.save(path.join(output_dir, 'D.npy'), d_matrix)
